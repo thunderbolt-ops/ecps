7bd8a09ab48054836af6b5a12e4c0be7fa74e0dd
thunderbolt-ops <itpro0707@gmail.com>
2026-02-06 01:28:55 +0530

    Add Prometheus and Grafana observability stack via Terraform

---
+++ infra/envs/dev/observability.tf
@@ -0,0 +1,46 @@
+resource "helm_release" "kube_prometheus_stack" {
+  name       = "kube-prometheus-stack"
+  namespace  = kubernetes_namespace.platform_observability.metadata[0].name
+  repository = "https://prometheus-community.github.io/helm-charts"
+  chart      = "kube-prometheus-stack"
+  version    = "61.2.0"
+
+  create_namespace = false
+
+  depends_on = [
+    kubernetes_namespace.platform_observability
+  ]
+
+  values = [
+    yamlencode({
+      prometheus = {
+        prometheusSpec = {
+          retention = "6h"
+        }
+      }
+
+      grafana = {
+        enabled = true
+
+        adminUser     = "admin"
+        adminPassword = "admin"
+
+        service = {
+          type = "ClusterIP"
+        }
+      }
+
+      alertmanager = {
+        enabled = true
+      }
+
+      kubeStateMetrics = {
+        enabled = true
+      }
+
+      nodeExporter = {
+        enabled = true
+      }
+    })
+  ]
+}
